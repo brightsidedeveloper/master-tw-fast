828070c80c36ddc1ddefd885312efdd340fd7bc7
Tim <tim@brightsidedeveloper.com>
2023-03-01 22:37:43 -0600

    More ideas flowing

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from "./App"
 import YourApp from "./YourApp"
 import FromScratch from "./FromScratch"
 
+//* You can Keep App as a Referenece
+//* You can use YourApp is a copy to play around with
+//* You can use FromScratch to start from scratch (make your own project or create a design like this one)
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
